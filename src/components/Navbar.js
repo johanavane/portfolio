5f52843c72c49214d9fcab43c1fa48a45f273188
Jovmarti <jovmarti@ucsc.edu>
2021-09-14 18:45:39 -0700

    Added working links to pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React from "react";
 import * as FaIcons from "react-icons/fa";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import Hamburger from "./Hamburger";
 
 function Navbar() {
   return (
     <div>
       <div className="navbar">
-        <Link to="#" className="menu-bars"></Link>
-        <FaIcons.FaBars />
+        {/* <Link to="#" className="menu-bars"></Link> */}
+        <button>
+          <FaIcons.FaBars />
+        </button>
       </div>
+      <Hamburger />
     </div>
   );
 }
