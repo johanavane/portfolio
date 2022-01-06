18d9a832f028d5955988a755984690c7161cb44b
Jovmarti <jovmarti@ucsc.edu>
2022-01-05 20:05:10 -0800

    not sure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,39 @@ import React from "react";
 import "../styles/Home.css";
 import "@fontsource/shrikhand";
 
-function home() {
-  return (
-    <div className="home-container">
-      <div className="home-wrapper">
-        <div className="home-text">
-          <a>JOHANA.</a>
+// defining compoennt as a class
+// in which render() must be defined
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="home-container">
+        <div className="home-wrapper">
+          <div className="home-text">
+            <a>JOHANA.</a>
+          </div>
+          {/* <div className="welcome">
+            <p> Welcome to my site.</p>
+          </div> */}
         </div>
-        {/* <div className="welcome">
-          <p> Welcome to my site.</p>
-        </div> */}
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default home;
+export default Home;
+
+// Reacts lets you define componets are classes or fucntions
+// function home() {
+//   return (
+//     <div className="home-container">
+//       <div className="home-wrapper">
+//         <div className="home-text">
+//           <a>JOHANA.</a>
+//         </div>
+//         {/* <div className="welcome">
+//           <p> Welcome to my site.</p>
+//         </div> */}
+//       </div>
+//     </div>
+//   );
+// }
