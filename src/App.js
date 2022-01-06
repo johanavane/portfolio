4f42b8d0c3a452c07913d0cea91854bc55d00201
Jovmarti <jovmarti@ucsc.edu>
2022-01-05 21:42:48 -0800

    created loading page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -5,29 +6,51 @@ import About from "./pages/About";
 import Resume from "./pages/Resume";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/About" exact component={About} />
-          <Route path="/Resume" exact component={Resume} />
-        </Switch>
-      </Router>
-    </>
-  );
+class App extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.fakeRequest().then(() => {
+      const el = document.querySelector(".loader-container");
+      if (el) {
+        el.remove(); // removing the spinner element
+        this.setState({ loading: false }); // showing the app
+      }
+    });
+  }
+
+  fakeRequest = () => {
+    return new Promise((resolve) => setTimeout(() => resolve(), 2500));
+  };
+
+  render() {
+    if (this.state.loading) {
+      return null; //app is not ready (fake request is in process)
+    }
+
+    return (
+      <>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/About" exact component={About} />
+            <Route path="/Resume" exact component={Resume} />
+          </Switch>
+        </Router>
+      </>
+    );
+  }
 }
 
 export default App;
 
-{
-  // react router dom allows us to have diff pages in app
-  // Router = tell react we want router in app
-  // Navbar is a component
-  // Will be in every single page
-  // Switch = we want to render one route at a time
-  // Inside switch we define the diff pages we will be having
-  // path = destination of a Route
-}
+// react router dom allows us to have diff pages in app
+// Router = tell react we want router in app
+// Navbar is a component
+// Will be in every single page
+// Switch = we want to render one route at a time
+// Inside switch we define the diff pages we will be having
+// path = destination of a Route
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,28 @@ import "@fontsource/shrikhand";
 // defining compoennt as a class
 // in which render() must be defined
 class Home extends React.Component {
+  // state = {
+  //   loading: true,
+  // };
+
+  // componentDidMount() {
+  //   this.fakeRequest().then(() => {
+  //     const el = document.querySelector(".loader-container");
+  //     if (el) {
+  //       el.remove(); // removing the spinner element
+  //       this.setState({ loading: false }); // showing the app
+  //     }
+  //   });
+  // }
+
+  // fakeRequest = () => {
+  //   return new Promise((resolve) => setTimeout(() => resolve(), 2500));
+  // };
+
   render() {
+    // if (this.state.loading) {
+    //   return null; //app is not ready (fake request is in process)
+    // }
     return (
       <div className="home-container">
         <div className="home-wrapper">
